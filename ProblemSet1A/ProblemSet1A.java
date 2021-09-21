4f16e849c179e120830a881655212f6089b12402
clara chen <59670258+clarachenn@users.noreply.github.com>
2021-09-20 19:31:45 -0700

    Add files via upload

--- ProblemSet1A/ProblemSet1A.java
+++ ProblemSet1A/ProblemSet1A.java
@@ -34,16 +34,44 @@ class ProblemSet1A {
 
     //Sum of arithmetic series:
     double a1 = 1.0; //first term
-    double ak = 5; //last term
     double k = 5; //number of terms
+    double difference = 1; //the difference between each number
+    double ak = a1 + (k - 1) * difference; //last term
+    
+    double sum = k/2 * (a1 + ak); //finds the sum of the arithmetic series
+     
+//-------------------------------------------------------------------------------------------------------------------
 
-    double sk = k/2 * (a1 + ak);
+    //Sum of geometric series
+    double g1 = 3.0;
+    double kTerms = 3;
+    double r = 2.0;
+    double gSum = g1 * ((1 - Math.pow(r, kTerms)) / (1 - r));
     
+//-------------------------------------------------------------------------------------------------------------------
 
-System.out.println("QUADRATIC FORMULA");
-System.out.println("The Solutions for " + (int)a + "x^2 + " + (int)b + "x + " + (int)c + " are " + firstX + " and " + secondX + ".");
-    
-    
-    
+System.out.println("QUADRATIC FORMULA"); //prints "QUADRATIC FORMULA"
+System.out.println("The Solutions for " + (int)a + "x^2 + " + (int)b + "x + " + (int)c + " are " + firstX + " and " + secondX + "."); //prints the answer for the quadratic formula question
+
+System.out.println();
+
+System.out.println("SLOPE FORMULA"); //prints "SLOPE FORMULA"
+System.out.println("A line connecting the points (" + (int)x1 + ", " + (int)y1 + ") " + "and (" + (int)x2 + ", " + (int)y2 + ") has a slope of " + m) ; //prints the answer for the slope question
+
+System.out.println();
+
+System.out.println("MIDPOINT FORMULA"); //prints "MIDPOINT FORMULA"
+System.out.println("The midpoint between (" + (int)x1 + ", " + (int)y1 + ") " + "and (" + (int)x2 + ", " + (int)y2 + ") is (" + M1 + ", " + M2 + ")"); //prints the answer for the midpoint question
+ 
+System.out.println();
+
+System.out.println("SUM OF AN ARITHMETIC SERIES"); //prints "SUM OF AN ARITHMETIC SERIES"
+System.out.println("The sum of the first " + (int)k + " terms of an arithmetic series that starts with " + a1 + " and increases by " + difference + " is " + sum); //prints the answer to the arithmetic series question
+
+System.out.println();
+
+System.out.println("SUM OF A FINITE GEOMETRIC SERIES"); //prints "SUM OF A FINITE GEOMETRIC SERIES"
+System.out.println("The sum of the first " + kTerms + " terms of a finite geometric series that starts with " + g1 + " and increases by a rate of " + r + " is " + gSum); //prints the answer to the geometric series question
+
     }
 }
